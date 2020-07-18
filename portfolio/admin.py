70ccf278c76141d996074a989fda725298b729b5
Jebin Philipose <jebinphilip24@gmail.com>
2020-07-18 15:31:47 +0530

    Revamp about page

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from .models import Degree, Skill, Summary, Project
+from .models import Project
 
-admin.site.register(Summary)
-admin.site.register(Skill)
-admin.site.register(Degree)
 admin.site.register(Project)
